8b0441e4bd618342893d7b0e58e1cd99876787ee
Nikki <suiguo3564@gmail.com>
2022-12-21 19:20:25 +0800

    测试代码

--- telegram_bot/bot_test.go
+++ telegram_bot/bot_test.go
@@ -1,19 +1,16 @@
-package telegrambot_test
+package telegrambot
 
 import (
 	"fmt"
-	"testing"
-
-	"github.com/suiguo/telegrambot"
 )
 
-func TestBot(t *testing.T) {
-	bot, err := telegrambot.NewBot("5904746042:AAGjBMN_ahQ0uavSCakrEFUN7RV2Q8oDY4I")
+func BotTest() {
+	bot, err := NewBot("5904746042:AAGjBMN_ahQ0uavSCakrEFUN7RV2Q8oDY4I")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	err = telegrambot.SendMsg(832328501, "test")
+	err = bot.SendMsg(832328501, "test")
 	if err != nil {
 		fmt.Println(err)
 	}
