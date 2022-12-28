fec5dada4ec374154ca4ada00d3140d09d15b509
Nikki <suiguo3564@gmail.com>
2022-12-28 19:05:48 +0800

    new bot s

--- telegram_bot/telegram_bot.go
+++ telegram_bot/telegram_bot.go
@@ -8,6 +8,8 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var apiMap sync.Map
+
 type Bot interface {
 	SendMsg(chat_id int64, msg string) error
 	SendMsgByUser(user string, msg string) error
@@ -52,7 +54,21 @@ func (b *bot) Init(api_key string) error {
 	return nil
 }
 
-func NewBot(api_key string) (Bot, error) {
+func NewBot(api_key string, newInstance ...bool) (Bot, error) {
+	if len(newInstance) > 0 && newInstance[0] { //填了数据返回全新的api实例
+		tmp := &bot{}
+		apiMap.Store(api_key, tmp)
+		return tmp, tmp.Init(api_key)
+	}
+	single, ok := apiMap.Load(api_key)
+	if ok {
+		return single.(*bot), nil
+	}
 	tmp := &bot{}
-	return tmp, tmp.Init(api_key)
+	err := tmp.Init(api_key)
+	if err == nil {
+		apiMap.Store(api_key, tmp)
+		return tmp, nil
+	}
+	return nil, err
 }
